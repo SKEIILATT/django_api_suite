e8cd2608a155b0989bf717d63bc5320039da5f13
JairPalaguachi <jpalagua@espol.edu.ec>
2025-07-28 19:54:40 +0000

    Guia21

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import credentials
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "landing_api",
+    "firebase_admin",
     "rest_framework",
     'demo_rest_api',
     "homepage",
@@ -130,3 +134,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CREDENTIALS_PATH, {
+   'databaseURL': 'https://console.firebase.google.com/project/taxpro-8d340/database/taxpro-8d340-default-rtdb/data/~2F'
+})
